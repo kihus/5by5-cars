0a6b804a58e27b2ac5018b22cbc6743e1efdcdde
Bruno Nascimento <Bruno Nascimento@DESKTOP-3M9QPCB>
2025-10-22 09:42:38 -0300

    Add project files.

---
+++ ProjCarros/Carro.cs
@@ -0,0 +1,43 @@
+﻿namespace ProjCarros;
+
+internal class Carro
+{
+    public int Id { get; private set; }
+    public string Marca { get; private set; }
+    public string Modelo { get; private set; }
+    public int Ano { get; private set; }
+    public string Cor { get; private set; }
+    public string Placa { get; private set; }
+
+    public Carro(
+        int id,
+        string marca,
+        string modelo,
+        int ano,
+        string cor,
+        string placa
+        )
+    {
+        Id = id;
+        Marca = marca;
+        Modelo = modelo;
+        Ano = ano;
+        Cor = cor;
+        Placa = placa;
+    }
+
+    public string ToFile()
+    {
+        return $"{Id},{Marca},{Modelo},{Ano},{Cor},{Placa}";
+    }
+
+    public override string ToString()
+    {
+        return $"Id: {Id}\n\r" +
+            $"Marca: {Marca}\n\r" +
+            $"Modelo: {Modelo}\n\r" +
+            $"Ano: {Ano}\n\r" +
+            $"Cor: {Cor}\n\r" +
+            $"Placa: {Placa}\n\r";
+    }
+}
